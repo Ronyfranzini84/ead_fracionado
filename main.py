5455d0a5db55ab341c27faa198aa90bb7a779e97
Ronyfranzini84 <ro.franzini2@gmail.com>
2026-03-24 20:57:58 -0300

    Instalando o kivy, homepage e ajustes page

---
+++ main.py
@@ -0,0 +1,21 @@
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.screenmanager import Screen
+
+class HomePage(Screen):
+    pass
+
+class AjustesPage(Screen):
+    pass
+
+GUI = Builder.load_file('main.kv') 
+
+class MainApp(App):
+
+    def build(self):
+        return GUI
+    
+    def on_start(self):
+        pass
+    
+MainApp().run()
