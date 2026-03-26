e3fe28f0cc10099da43617bf11ad43d2ca4473a1
RONALD THIAGO FRANZINI DE SOUZA <ronald.franzini@viavarejo.com.br>
2026-03-26 11:45:48 -0300

    Telas login e homepage iniciado

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
 from kivy.app import App
-from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
+from botoes import *
+import requests
+import os
+from functools import partial
+from datetime import date
+from kivy.uix.screenmanager import ScreenManager, Screen
 
-class HomePage(Screen):
+
+class LoginPage(Screen):   
     pass
 
-class AjustesPage(Screen):
+
+class HomePage(Screen, Image):
     pass
 
-GUI = Builder.load_file("main.kv")
 class MainApp(App):
-    def build(self):
-        return GUI
-    
     def mudar_tela(self, id_tela):
-        print(id_tela)
-        gerenciadir_telas = self.root.ids["screen_manager"]
-        gerenciadir_telas.current = id_tela
-    
+        self.root.ids["screen_manager"].current = id_tela
+
 MainApp().run()
